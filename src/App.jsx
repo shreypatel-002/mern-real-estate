f2f9ae39c605f09bbe55684202c105d91f9c5d90
shrey patel <shreypatel295@gmail.com>
2024-03-09 16:27:28 +0530

    Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import{BrowserRouter,Routes,Route} from "react-router-dom"
 import React from 'react'
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import SignUp from "./Pages/SignUp"
+import SignIn from "./Pages/SignIn"
+import Profile from "./Pages/Profile"
+import Header from "./components/Header"
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/SignUp' element={<SignUp/>}/>
+      <Route path='/SignIn' element={<SignIn/>}/>
+      <Route path='/Profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
+
